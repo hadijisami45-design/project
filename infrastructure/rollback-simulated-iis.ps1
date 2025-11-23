dcc7b859b50ff933709b4695324520ba6f55336e
sami hadiji <hadijisami45@gmail.com>
2025-11-23 19:51:24 +0100

    CICD done

--- infrastructure/rollback-simulated-iis.ps1
+++ infrastructure/rollback-simulated-iis.ps1
@@ -7,14 +7,14 @@ $targetPath = "_simulated_iis/Sites/SampleWebApp"
 
 Write-Host "Rollback de la version actuelle..."
 
-# Supprimer l'ancienne version
+
 if (Test-Path $targetPath) {
     Remove-Item $targetPath -Recurse -Force
 }
 
 Write-Host "Restauration depuis : $BackupPath"
 
-# Restaurer le dossier entier SampleWebApp depuis la sauvegarde
+
 Copy-Item $BackupPath $targetPath -Recurse -Force
 
 Write-Host "Rollback terminé avec succès !"
